ecf1e52eb69292d4d9cc7a9fa1105eced60d90d2
kgatman <makhatemakhate@gmail.com>
2025-01-02 15:01:23 +0200

    HTS-002 worked on to tally with HTS-001

--- openmrs/apps/reports/sql/hts_pitc_hiv_status_with_history_pivot.sql
+++ openmrs/apps/reports/sql/hts_pitc_hiv_status_with_history_pivot.sql
@@ -328,79 +328,6 @@ FROM (
                 ) AS HTSClients_HIV_Status
     ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
 
--- looking in 2385
-     UNION ALL
-
-  (SELECT Id, patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group,  'PITC' AS 'HIV_Testing_Initiation'
-        , 'Repeat' AS 'Testing_History' , HIV_Status, observation, sort_order
-    FROM
-            (select distinct patient.patient_id AS Id,
-                         patient_identifier.identifier AS patientIdentifier,
-                         concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
-                         floor(datediff(CAST("#endDate#" AS DATE), person.birthdate)/365) AS Age,
-                         (select name from concept_name cn where cn.concept_id = 1016 and concept_name_type='FULLY_SPECIFIED') AS HIV_Status,
-                         person.gender AS Gender,
-                         observed_age_group.name AS age_group,
-                         pitc.current_conc,
-                         SUBSTRING((CONCAT(o.obs_datetime, o.encounter_id)), 20) as observation,
-                         observed_age_group.sort_order AS sort_order
-
-            from obs o
-                -- HTS CLIENTS WITH HIV STATUS BY SEX AND AGE
-                 INNER JOIN patient ON o.person_id = patient.patient_id
-                 AND o.concept_id = 2165 and o.value_coded = 1016
-                 AND patient.voided = 0 AND o.voided = 0
-                  AND CAST(o.obs_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-                   AND CAST(o.obs_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-
-                 -- PROVIDER INITIATED TESTING AND COUNSELING
-                 LEFT Join (
-                  select distinct os.person_id, CAST(os.date_created as Date) as current_conc, os.encounter_id as encounter
-                  from obs os
-                  INNER JOIN patient ON os.person_id = patient.patient_id
-                  where os.concept_id = 2385 and os.value_coded = 1016
-                   AND CAST(os.obs_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-                   AND CAST(os.obs_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-                  AND patient.voided = 0 AND os.voided = 0
-                 ) as pitc
-                 on o.person_id = pitc.person_id
-                 AND o.encounter_id = pitc.encounter
-                 -- REPEATER, HAS HISTORY OF PREVIOUS TESTING
-                 LEFT Join (
-                  select distinct os.person_id, CAST(os.date_created as Date) as current_conc
-                  from obs os
-                  INNER JOIN patient ON os.person_id = patient.patient_id
-                  where os.concept_id = 2137 and os.value_coded = 2146
-                   AND CAST(os.obs_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-                   AND CAST(os.obs_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-                  AND patient.voided = 0 AND os.voided = 0
-                 ) as repeater
-                 on o.person_id = repeater.person_id
-                 and pitc.current_conc = repeater.current_conc
-
-				 -- Observation be in HIV Testing form
-                 LEFT join (
-                  select distinct os.person_id, CAST(os.date_created as Date) as current_conc
-                  from obs os
-                  INNER JOIN patient ON os.person_id = patient.patient_id
-                  where os.concept_id = 2385
-                   AND CAST(os.obs_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-                   AND CAST(os.obs_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-                  AND patient.voided = 0 AND os.voided = 0
-                 )as testingform
-                 on o.person_id = testingform.person_id
-
-                 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-                 INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
-                 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-                 INNER JOIN location l on o.location_id = l.location_id and l.retired=0
-                 INNER JOIN reporting_age_group AS observed_age_group ON
-                  CAST("#endDate#" AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-                  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-               WHERE observed_age_group.report_group_name = 'Modified_Ages'
-                ) AS HTSClients_HIV_Status
-    ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
--- 2385
   UNION ALL
   (SELECT Id, patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group,  'PITC' AS 'HIV_Testing_Initiation'
         , 'Repeat' AS 'Testing_History' , HIV_Status, observation, sort_order
@@ -546,7 +473,6 @@ FROM (
 
 			GROUP BY HTS_STATUS_DRVD_ROWS.age_group, HTS_STATUS_DRVD_ROWS.Gender
 			ORDER BY HTS_STATUS_DRVD_ROWS.sort_order)
-
 
 	UNION ALL
 
