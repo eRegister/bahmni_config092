9d055f7e9518d07effa90862e3d41048979c4cef
mzntlamelle <32940027+mzntlamelle@users.noreply.github.com>
2026-06-12 13:17:01 +0200

    Included Prep MMD in Prep Continuation clients
    Enhanced SQL for PrEP Current report to include PreP clients seen in the previous reporting period but still active

--- openmrs/apps/reports/sql/prep_curr_list.sql
+++ openmrs/apps/reports/sql/prep_curr_list.sql
@@ -1,97 +1,140 @@
-SELECT patientIdentifier , patientName, Age, Gender, age_group, 'PrEP_Continuation' AS 'Program_Status', Location
-	FROM
-                (select distinct patient.patient_id AS Id,
-									   patient_identifier.identifier AS patientIdentifier,
-									   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
-									   floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
-									   person.gender AS Gender,
-									   observed_age_group.name AS age_group,
-									   observed_age_group.sort_order AS sort_order,
-									   l.name AS Location
+SELECT patientIdentifier , patientName, Age, Gender, age_group, PrEP_Option, 'PrEP_New' AS Program_Status, Location
+FROM
+    (
+		select distinct 
+			patient.patient_id AS Id,
+			patient_identifier.identifier AS patientIdentifier,
+			concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+			TIMESTAMPDIFF(YEAR, person.birthdate, CAST('#endDate#' AS DATE)) AS Age,
+			person.gender AS Gender,
+			observed_age_group.name AS age_group, 
+			CASE prep_option.value_coded
+				WHEN 6096 THEN 'Daily'
+				WHEN 6097 THEN 'ED_PreP'
+				WHEN 6098 THEN 'Ring'
+				WHEN 6099 THEN 'Cab-La'
+				WHEN 6597 THEN 'Lenacapavir'
+				WHEN 4991 THEN 'Other'
+			END AS PrEP_Option, 
+			observed_age_group.sort_order AS sort_order,
+			l.name AS Location
 
-                from obs o
-						-- CLIENTS CONTINUING PrEP
-						inner join patient ON o.person_id = patient.patient_id 
-						and (o.concept_id = 5029
-								and CAST(o.obs_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-								and CAST(o.obs_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-						     )
-					    and patient.voided = 0 and o.voided = 0
-
-						and o.person_id not in 
-						(
-							 -- PrEP NEW
-							select distinct os.person_id 
-							from obs os
-								where os.concept_id = 4994
-								and CAST(os.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-								and CAST(os.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-								and os.voided = 0
-						)	
-
-						and o.person_id not in 
-						(
-							 -- Stopped PrEP
-							select distinct os.person_id 
-							from obs os
-								where os.concept_id = 5005
-								and CAST(os.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-								and CAST(os.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-								and os.voided = 0
-						)
-											 
-						 inner join person ON person.person_id = patient.patient_id and person.voided = 0
-						 inner join location l on o.location_id = l.location_id  and l.retired=0
-						 inner join person_name ON person.person_id = person_name.person_id and person_name.preferred = 1
-						 inner join patient_identifier ON patient_identifier.patient_id = person.person_id and patient_identifier.identifier_type = 3 and patient_identifier.preferred=1
-						 inner join reporting_age_group AS observed_age_group ON
-						  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-						  and (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-                where observed_age_group.report_group_name = 'Modified_Ages') AS PrEP_Seen
--- ORDER BY PrEP_Seen.patientName
-
-UNION
-
-SELECT patientIdentifier , patientName, Age, Gender, age_group, 'PrEP_New' AS 'Program_Status', Location
-	FROM
-                (select distinct patient.patient_id AS Id,
-									   patient_identifier.identifier AS patientIdentifier,
-									   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
-									   floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
-									   person.gender AS Gender,
-									   observed_age_group.name AS age_group,
-									   observed_age_group.sort_order AS sort_order,
-									   l.name AS Location
-
-                from obs o
-						-- CLIENTS NEWLY INITIATED
-						inner join patient ON o.person_id = patient.patient_id 
-						and (o.concept_id = 4994
-								and CAST(o.value_datetime AS DATE) >= CAST('#startDate#' AS DATE)
-								and CAST(o.value_datetime AS DATE) <= CAST('#endDate#' AS DATE)
-						     )
-					    and patient.voided = 0 and o.voided = 0
-						and o.person_id not in 
-						(
-							 -- TRANSFER INN
-							select distinct os.person_id 
-							from obs os
-							where os.concept_id = 5070
-							and os.value_coded = 2146
-						)	
-						and o.person_id not in 
-						(
-							 -- HAS BEEN ON PrEP BEFORE
-							select distinct os.person_id 
-							from obs os
-							where os.concept_id = 5003 and os.value_coded = 1
-						 )						 
-						 inner join person ON person.person_id = patient.patient_id and person.voided = 0
-						 inner join location l on o.location_id = l.location_id  and l.retired=0
-						 inner join person_name ON person.person_id = person_name.person_id and person_name.preferred = 1
-						 inner join patient_identifier ON patient_identifier.patient_id = person.person_id and patient_identifier.identifier_type = 3 and patient_identifier.preferred=1
-						 inner join reporting_age_group AS observed_age_group ON
-						  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-						  and (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-                where observed_age_group.report_group_name = 'Modified_Ages') AS Newly_Initiated_Clients
+    	from obs o
+		-- CLIENTS NEWLY INITIATED
+		inner join patient ON o.person_id = patient.patient_id 
+			and (o.concept_id = 4994
+					and o.value_datetime >= '#startDate#'
+					and o.value_datetime < DATE_ADD('#endDate#', INTERVAL 1 DAY)
+				)
+			and patient.voided = 0 and o.voided = 0
+			and o.person_id not in 
+				(
+					-- TRANSFER INN
+				select distinct os.person_id 
+				from obs os
+				where os.concept_id = 5070
+					and os.value_coded = 2146
+				)	
+			and o.person_id not in 
+				(
+				-- HAS BEEN ON PrEP BEFORE
+				select distinct os.person_id 
+				from obs os
+				where os.concept_id = 5003 and os.value_coded = 1
+				)						 
+			inner join person ON person.person_id = patient.patient_id and person.voided = 0
+				LEFT JOIN obs prep_option ON prep_option.person_id = o.person_id 
+			AND prep_option.voided = 0 
+			AND prep_option.concept_id = 6100					 
+			inner join location l on o.location_id = l.location_id  and l.retired=0
+			inner join person_name ON person.person_id = person_name.person_id and person_name.preferred = 1
+			inner join patient_identifier ON patient_identifier.patient_id = person.person_id and patient_identifier.identifier_type = 3 and patient_identifier.preferred=1
+			inner join reporting_age_group AS observed_age_group ON
+				CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+					and (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+		where observed_age_group.report_group_name = 'Modified_Ages'
+	) AS Newly_Initiated_Clients
 -- ORDER BY Newly_Initiated_Clients.patientName
+
+UNION ALL
+SELECT patientIdentifier , patientName, Age, Gender, age_group, PrEP_Option, 'PrEP_Continuation' AS Program_Status, Location
+FROM
+    (SELECT DISTINCT 
+		patient.patient_id AS Id,
+		patient_identifier.identifier AS patientIdentifier,
+		concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+		TIMESTAMPDIFF(YEAR, person.birthdate, CAST('#endDate#' AS DATE)) AS Age,
+		person.gender AS Gender,
+		observed_age_group.name AS age_group,
+		CASE prep_option.value_coded
+			WHEN 6096 THEN 'Daily'
+			WHEN 6097 THEN 'ED_PreP'
+			WHEN 6098 THEN 'Ring'
+			WHEN 6099 THEN 'Cab-La'
+			WHEN 6597 THEN 'Lenacapavir'
+			WHEN 4991 THEN 'Other'
+		END AS PrEP_Option,
+		observed_age_group.sort_order AS sort_order,
+		l.name AS Location
+
+    FROM obs o
+	-- CLIENTS CONTINUING PrEP
+	INNER JOIN patient ON o.person_id = patient.patient_id 
+		AND o.concept_id = 5029	
+		AND patient.voided = 0 
+		AND o.voided = 0
+		AND o.obs_datetime < DATE_ADD('#endDate#', INTERVAL 1 DAY)
+	INNER JOIN obs active_prep ON active_prep.person_id = o.person_id
+		AND active_prep.concept_id = 3752
+		AND active_prep.encounter_id = o.encounter_id
+		AND active_prep.value_datetime >= '#endDate#'	
+		AND active_prep.voided = 0	
+		LEFT JOIN obs prep_option
+    ON prep_option.obs_id =
+    (
+        SELECT po.obs_id
+        FROM obs po
+        WHERE po.person_id = o.person_id
+          AND po.encounter_id = o.encounter_id
+          AND po.concept_id = 6100
+          AND po.voided = 0
+        ORDER BY po.obs_datetime DESC, po.obs_id DESC
+        LIMIT 1
+    )/*
+	LEFT JOIN obs prep_option ON prep_option.person_id = o.person_id
+   		AND prep_option.encounter_id = o.encounter_id
+   		AND prep_option.concept_id = 6100
+   		AND prep_option.voided = 0		*/				 
+	INNER JOIN person ON person.person_id = patient.patient_id 
+		AND person.voided = 0
+	INNER JOIN location l on active_prep.location_id = l.location_id
+		AND l.retired=0
+	INNER JOIN person_name ON person.person_id = person_name.person_id 
+		AND person_name.preferred = 1
+	INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id 
+		AND patient_identifier.identifier_type = 3 	
+		AND patient_identifier.preferred=1
+	INNER JOIN reporting_age_group AS observed_age_group ON
+		CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+			AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+    WHERE observed_age_group.report_group_name = 'Modified_Ages'
+	AND NOT EXISTS (
+          SELECT 1
+          FROM obs os
+          WHERE os.person_id = o.person_id
+            AND os.concept_id = 4994
+            AND os.obs_datetime >= '#startDate#'
+            AND os.obs_datetime < DATE_ADD('#endDate#', INTERVAL 1 DAY)
+            AND os.voided = 0
+      )
+      AND NOT EXISTS (
+          SELECT 1
+          FROM obs os
+          WHERE os.person_id = o.person_id
+            AND os.concept_id = 5005
+            AND os.obs_datetime >= '#startDate#'
+            AND os.obs_datetime < DATE_ADD('#endDate#', INTERVAL 1 DAY)
+            AND os.voided = 0
+      )
+	) AS PrEP_Seen
+
